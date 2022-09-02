fa84a8eac1d9187916c2549f65e3a91dee591ee0
otenaike oluwatiobi <64742528+sylvercodez@users.noreply.github.com>
2022-09-02 02:16:32 +0100

    Update Updateprofile.vue

--- src/pages/Updateprofile.vue
+++ src/pages/Updateprofile.vue
@@ -91,53 +91,8 @@
              
             </div>
           </div>
-          <div class="input-wrap">
-            <label class="form-det" for=""
-              >Are you authorized to legally work in the U.S.?
-            </label>
-            <br />
-            <div class="input">
-              <i class="ri-flag-fill q-mr-md icon-enroll"></i>
-
-              <select name="can_work_in_usa" v-model="can_work_in_usa" id="">
-                <option value="Yes">Yes</option>
-                <option value="No">No</option>
-              </select>
-            </div>
-
-            <span
-              v-if="
-                inputErr ===
-                'Missing Fields! Please choose if you can work in USA'
-              "
-              class="error"
-              >{{ inputErr }}</span
-            >
-          </div>
           <div class="two">
-            <div class="input-wrap">
-              <label class="form-det" for=""
-                >City, State of Residence (e.g. Seattle, WA)
-              </label>
-              <br />
-
-              <div class="input">
-                <i class="ri-time-fill q-mr-md icon-enroll"></i>
-
-                <input
-                  type="text"
-                  name="city"
-                  v-model="timezone"
-                  placeholder=""
-                />
-              </div>
-              <span
-                v-if="inputErr === 'Missing Fields! City and State required'"
-                class="error"
-                >{{ inputErr }}</span
-              >
-            </div>
-            <div class="input-wrap">
+          <div class="input-wrap">
               <label class="form-det" for="">Phone Number</label> <br />
 
               <div class="input">
@@ -156,6 +111,132 @@
                 >{{ inputErr }}</span
               >
             </div>
+          <div class="input-wrap">
+            <label class="form-det" for=""
+              >, State of Residence 
+            </label>
+            <br />
+            <div class="input">
+              <i class="ri-flag-fill q-mr-md icon-enroll"></i>
+
+              <select name="state" v-model="state" id="">
+              <option value="AL">Alabama</option>
+	<option value="AK">Alaska</option>
+	<option value="AZ">Arizona</option>
+	<option value="AR">Arkansas</option>
+	<option value="CA">California</option>
+	<option value="CO">Colorado</option>
+	<option value="CT">Connecticut</option>
+	<option value="DE">Delaware</option>
+	<option value="DC">District Of Columbia</option>
+	<option value="FL">Florida</option>
+	<option value="GA">Georgia</option>
+	<option value="HI">Hawaii</option>
+	<option value="ID">Idaho</option>
+	<option value="IL">Illinois</option>
+	<option value="IN">Indiana</option>
+	<option value="IA">Iowa</option>
+	<option value="KS">Kansas</option>
+	<option value="KY">Kentucky</option>
+	<option value="LA">Louisiana</option>
+	<option value="ME">Maine</option>
+	<option value="MD">Maryland</option>
+	<option value="MA">Massachusetts</option>
+	<option value="MI">Michigan</option>
+	<option value="MN">Minnesota</option>
+	<option value="MS">Mississippi</option>
+	<option value="MO">Missouri</option>
+	<option value="MT">Montana</option>
+	<option value="NE">Nebraska</option>
+	<option value="NV">Nevada</option>
+	<option value="NH">New Hampshire</option>
+	<option value="NJ">New Jersey</option>
+	<option value="NM">New Mexico</option>
+	<option value="NY">New York</option>
+	<option value="NC">North Carolina</option>
+	<option value="ND">North Dakota</option>
+	<option value="OH">Ohio</option>
+	<option value="OK">Oklahoma</option>
+	<option value="OR">Oregon</option>
+	<option value="PA">Pennsylvania</option>
+	<option value="RI">Rhode Island</option>
+	<option value="SC">South Carolina</option>
+	<option value="SD">South Dakota</option>
+	<option value="TN">Tennessee</option>
+	<option value="TX">Texas</option>
+	<option value="UT">Utah</option>
+	<option value="VT">Vermont</option>
+	<option value="VA">Virginia</option>
+	<option value="WA">Washington</option>
+	<option value="WV">West Virginia</option>
+	<option value="WI">Wisconsin</option>
+	<option value="WY">Wyoming</option>
+              </select>
+            </div>
+
+            <span
+              v-if="
+                inputErr ===
+                ''Missing Fields! State required''
+              "
+              class="error"
+              >{{ inputErr }}</span
+            >
+          </div>
+          </div>
+          <div class="two">
+            <div class="input-wrap">
+              <label class="form-det" for=""
+                >City (e.g. California, Illinois)
+              </label>
+              <br />
+
+              <div class="input">
+                <i class="ri-time-fill q-mr-md icon-enroll"></i>
+
+                <input
+                  type="text"
+                  name="city"
+                  v-model="timezone"
+                  placeholder=""
+                />
+              </div>
+              <span
+                v-if="inputErr === 'Missing Fields! City required'"
+                class="error"
+                >{{ inputErr }}</span
+              >
+            </div>
+            <div class="input-wrap">
+              <label class="form-det" for=""
+                >Minority Representation: I identify as</label
+              >
+              <br />
+
+              <div class="input">
+                <i class="ri-apps-fill q-mr-md icon-enroll"></i>
+
+                <select name="representation" v-model="representation" id="">
+                  <option value="native american">
+                    American Indian or Alaskan Native
+                  </option>
+
+                  <option value="black">Black (not of Hispanic origin)</option>
+                  <option value="hispanic">
+                    Hispanic (Including persons of Mexican, Puerto RIcan, Cuban,
+                    and Central or South American origin
+                  </option>
+                  <option value="pacific islander">Pacific Islander</option>
+                  <option value="other">Other</option>
+                </select>
+              </div>
+              <span
+                v-if="inputErr === 'Missing Fields! Representation is required'"
+                class="error"
+                >{{ inputErr }}</span
+              >
+            </div>
+            
           </div>
 
           <div class="two">
@@ -743,6 +824,7 @@ export default {
       form: {
         age_group: this.userData("age_group"),
         can_work_in_usa: this.userData("can_work_in_usa"),
+        state: this.userData("state"),
         email: this.userData("email"),
         employment_status: this.userData("employment_status"),
         gender: this.userData("gender"),
@@ -868,7 +950,7 @@ export default {
       console.log("first");
       const timezone = this.timezone;
       const city = this.timezone;
-      const state = this.timezone;
+      const state = this.state;
       const name = this.form.name;
       const email = this.form.email;
       const linkedin_url = this.linkedin_url;
@@ -943,6 +1025,7 @@ export default {
       formDataa.append("employment_status", employment_status);
       formDataa.append("linkedin_url", linkedin_url);
       formDataa.append("city", timezone);
+       formDataa.append("state", state);
       formDataa.append("email", email);
       formDataa.append("referral", referral);
       formDataa.append("learning_track", learning_track);
